420e0699d441c4ef105c36f4ff568a6ce7b7c687
Silvan Buedenbender <silvancodes@gmail.com>
2025-10-23 20:52:09 +0000

    Modify pipeline to keep input

--- gpn/pipeline/gpn_pipeline.py
+++ gpn/pipeline/gpn_pipeline.py
@@ -3,19 +3,29 @@ from transformers import Pipeline
 
 
 class GPNPipeline(Pipeline):
+    def _sanitize_parameters(self, **kwargs):
+        preprocess_kwargs = {}
+        postprocess_kwargs = {}
+        return preprocess_kwargs, {}, postprocess_kwargs
+
     def preprocess(self, seq):
-        return self.tokenizer(
+        tokens = self.tokenizer(
             seq,
             return_tensors="pt",
             return_attention_mask=False,
             return_token_type_ids=False,
         )
+        return {"tokens": tokens, "seq": seq}
 
     def _forward(self, model_inputs):
-        return self.model(input_ids=model_inputs["input_ids"])
+        output = self.model(input_ids=model_inputs["tokens"]["input_ids"])
+        return {"output": output, "seq": model_inputs["seq"]}
 
     def postprocess(self, model_outputs):
         acgt_idxs = [self.tokenizer.get_vocab()[nuc] for nuc in list("acgt")]
-        nucleotide_logits = model_outputs.logits[:, :, acgt_idxs]
+        nucleotide_logits = model_outputs["output"].logits[:, :, acgt_idxs]
         output_probs = torch.nn.functional.softmax(nucleotide_logits, dim=-1)
+
+        print(model_outputs["seq"])
+
         return output_probs
